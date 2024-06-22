967b5afa6162f2434b36b7625ee68e6e130e25da
Victor-arch671 <sadoluwaprecious0@gmail.com>
2024-06-22 10:58:19 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from flask import Flask, request, render_template
+from twilio.twiml.messaging_response import MessagingResponse
+from googlesearch import search
 
-from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+@app.route("/", methods=["GET", "POST"])
+def search_page():
+    if request.method == "POST":
+        user_msg = request.form.get('search_query', '').lower()
+        if not user_msg:
+            return render_template('search.html', results=None)
+
+        response = MessagingResponse()
+        q = user_msg + " site:google.com"
+        result = [url for url in search(q, num_results=3)]
+
+        return render_template('search.html', results=result)
+
+    return render_template('search.html', results=None)
+
+@app.route("/result")
+def result_page():
+    url = request.args.get('url')
+    return render_template('result.html', url=url)
+
+if __name__ == "__main__":
+    app.run(debug=True)
